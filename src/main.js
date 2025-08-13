7b97cdb45c4134f49a5efcef2b1426e7ea2bfc8c
Tejaswi P <tejaswi@mobil80.com>
2025-08-13 12:48:26 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,9 +11,9 @@ async function initApp() {
   Amplify.configure({
     Auth: {
       Cognito: {
-        identityPoolId: "us-east-1:5d51df7e-d160-43db-bed7-0f636d93fd76",
-        userPoolId: "us-east-1_XHrv2PFvB",
-        userPoolClientId: "3480qig1bcujchnuj3g9shogou",
+        identityPoolId: "",
+        userPoolId: "",
+        userPoolClientId: "",
         loginWith: {
           email: true,
         },
@@ -22,16 +22,16 @@ async function initApp() {
     },
     Storage: {
       S3: {
-        bucket: "slts-app-users",
-        region: "us-east-1",
+        bucket: "",
+        region: "",
       },
     },
     API: {
       GraphQL: {
         endpoint:
-          "https://3aoybc333vbffmquaojhw4aasa.appsync-api.us-east-1.amazonaws.com/graphql",
-        region: "us-east-1",
-        defaultAuthMode: "userPool", // userPool = Cognito User Pools
+          "",
+        region: "",
+        defaultAuthMode: "", // userPool = Cognito User Pools
       },
     },
   });
